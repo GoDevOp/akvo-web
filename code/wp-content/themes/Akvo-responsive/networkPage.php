e9a1a51d4ddc2b0486ee6926e9401ea360b855e8
Gabriel von Heijne <gabriel@akvo.org>
2014-09-23 11:56:00 +0200

    [#77] Example KPI charts

--- code/wp-content/themes/Akvo-responsive/networkPage.php
+++ code/wp-content/themes/Akvo-responsive/networkPage.php
@@ -54,78 +54,222 @@
          class="tooltips moreLink">info</a> <a href="" class="moreLink darkBg  hidden">See more</a>
       </li>
 
-      <li class="dashSingle" id="flowDummy"></li>
-
-      <li class="dashSingle" id="flowChart"></li>
-
+      <li class="dashSingle" id="kpiChart"></li>
       <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 
       <script type="text/javascript">
         google.load('visualization', '1', {'packages' : ['corechart']});
-        google.setOnLoadCallback(function() { drawChart('') });
+        google.setOnLoadCallback(function() { drawChart() });
 
-        var dataSourceUrl = 'https://spreadsheets.google.com/tq?key=1G9wXq8cSKSExacDlizoWSoW1PDIEVkki7lMi9M7Q6bc&pub=1';
+//        var dataSourceUrl = 'https://spreadsheets.google.com/tq?key=1G9wXq8cSKSExacDlizoWSoW1PDIEVkki7lMi9M7Q6bc&pub=1';
+//        var dataSourceUrl = 'https://spreadsheets.google.com/tq?key=1DliXl5driOAoHDhRgPOQKigACEI4-pdF42mZsHm_EmA&pub=1&range=A1:A13,F1:F13,F14:F26';
+
 
         function drawChart() {
-          var query = new google.visualization.Query(dataSourceUrl);
-          query.send(handleQueryResponse);
-        }
+          var charts = [
+            {
+              'name': 'kpiChart',
+              'dataSourceUrl': 'https://spreadsheets.google.com/tq?key=1DliXl5driOAoHDhRgPOQKigACEI4-pdF42mZsHm_EmA&pub=1&range=A1:A13,F1:F13,F14:F26',
+              'visualization': google.visualization.LineChart,
+              'options': kpiOptions
+            },
+            {
+              'name': 'flowChart',
+              'dataSourceUrl': 'https://spreadsheets.google.com/tq?key=1G9wXq8cSKSExacDlizoWSoW1PDIEVkki7lMi9M7Q6bc&pub=1',
+              'visualization': google.visualization.BarChart,
+              'options': flowOptions
+            },
+            {
+              'name': 'contractChart',
+              'dataSourceUrl': 'https://spreadsheets.google.com/tq?key=1DliXl5driOAoHDhRgPOQKigACEI4-pdF42mZsHm_EmA&pub=1&range=A1:A13,C1:C13,C14:C26',
+              'visualization': google.visualization.LineChart,
+              'options': contractOptions
+            },
+            {
+              'name': 'resultsChart',
+              'dataSourceUrl': 'https://spreadsheets.google.com/tq?key=1xhEs8bn9_QqushURNcdrpWjLwIOATnBQLPxkEAk7g5s&pub=1&range=B1:B16,E1:E16,C1:C16',
+              'visualization': google.visualization.LineChart,
+              'options': resultsOptions
+            }
+          ];
 
-        function handleQueryResponse(response) {
-          // get (incomplete) info from the style sheets
-          var barColor = $(".dashData.flowData li").css("color");
-          var font = $(".dashData.flowData li").css("font-family");
-
-          //The google spreadsheet data
-          var data = response.getDataTable();
-
-          // create x-axis labels from first column in the spreadsheet
-          var arrayLength = data.tf.length;
-          var tics = [];
-          for (i=0; i<arrayLength; i++  ) {
-            tics[i] = data.tf[i].c[0].v;
+          for (var i=0; i<charts.length; i++) {
+            var query = new google.visualization.Query(charts[i].dataSourceUrl);
+            function wrapper(i) {
+              return function(dataFromServer) {
+                handleQueryResponse(dataFromServer, i);
+              }
+            }
+            query.send(wrapper(i));
           }
-          var chart = new google.visualization.BarChart(document.getElementById('flowChart'));
-          var options = {
-            title: "AKVO FLOW SURVEY COUNT",
-            titlePosition: 'in', // chart title within the chart area
-            titleTextStyle: {
-              color: '#e77c00',
-              fontName: font,
-              fontSize: 12,
-              bold: false,
-              italic: false
-            },
-            orientation: 'horizontal', // vertical bars
-            backgroundColor: '#19191c', // color of the whole div, TODO: couldn't get this from css
-            chartArea: {
-              backgroundColor: '#231d1a', // color of the actual chart, TODO: couldn't get this from css
-              left:'15%', top: '10%',
-              width:'70%', height:'72%'
-            },
-            colors:[barColor], // color of the actual bars
-            hAxis: {
-              ticks: tics, // x-axis labels
-              gridlines: {color: '#3d352e'}, // vertical grid lines color
-              textStyle: { //x-axis labels styling
-                color: '#574F48',
-                fontName: font
-              }
-            },
-            vAxis: {
-              gridlines: {color: '#3d352e'}, //horizontal grid lines color
-              textStyle: { //y-axis labels styling
-                color: '#574F48',
-                fontName: font
-              }
-            },
-            height: 350,
-            legend: {position: 'none'} // remove bar legend
-          };
 
-          chart.draw(data, options);
+          function handleQueryResponse(response, count) {
+            var chart = charts[count]
+            var data = response.getDataTable();
+            var rendering = new chart.visualization(document.getElementById(chart.name));
+            var options = chart.options(data);
+            rendering.draw(data, options);
+          }
+
+          function baseOptions() {
+            var font = $(".dashData.flowData li").css("font-family");
+            var baseAxis = {
+              gridlines: {color: '#3d352e'}, // grid lines color
+              textStyle: { //labels styling
+                color: '#574F48',
+                fontName: font
+              }
+            }
+            return {
+              titlePosition: 'in', // chart title within the chart area
+              titleTextStyle: {
+                color: '#57c1e8',
+                fontName: font,
+                fontSize: 12,
+                bold: false,
+                italic: false
+              },
+              backgroundColor: '#19191c', // color of the whole div, TODO: couldn't get this from css
+              chartArea: {
+                backgroundColor: '#231d1a', // color of the actual chart, TODO: couldn't get this from css
+                left:'15%', top: '10%',
+                width:'70%', height:'72%'
+              },
+              hAxis: baseAxis,
+              vAxis: baseAxis,
+              height: 350
+            }
+          }
+
+          function kpiOptions(data) {
+            var font = $(".dashData.flowData li").css("font-family");
+            var options =  {
+              title: 'EXPECTED PIPELINE VALUE ACHIEVED',
+              curveType: 'function',
+              colors:['#57c1e8', '#bf83b9'], // color of the actual bars
+              legend: {
+                position: 'top',
+                textStyle: {
+                  color: '#574F48',
+                  fontName: font
+                }
+              },
+              vAxis:{
+                ticks: [
+                  {v:0.5, f:'50%'},
+                  {v:0.75, f:'75%'},
+                  {v:1.0, f:'100%'},
+                  {v:1.25, f:'125%'},
+                  {v:1.5, f:'150%'},
+                  {v:1.75, f:'175%'}
+                ]
+              }
+            };
+            return $.extend(true, {}, baseOptions(), options);
+          }
+
+          function flowOptions(data) {
+            // data: the google spreadsheet data "JSON"
+            // get (incomplete) info from the style sheets
+            var barColor = $(".dashData.flowData li").css("color");
+            var font = $(".dashData.flowData li").css("font-family");
+
+            // create x-axis labels from first column in the spreadsheet
+            var arrayLength = data.tf.length;
+            var tics = [];
+            for (i=0; i<arrayLength; i++  ) {
+              tics[i] = data.tf[i].c[0].v;
+            }
+            var options = {
+              title: "AKVO FLOW SURVEY COUNT",
+              titleTextStyle: {
+                color: '#e77c00',
+              },
+              orientation: 'horizontal', // vertical bars
+              colors:[barColor], // color of the actual bars
+              hAxis: {
+                ticks: tics // x-axis labels
+              },
+              legend: {position: 'none'} // remove bar legend
+            };
+            return $.extend(true, {}, baseOptions(), options);
+          }
+
+          function contractOptions(data) {
+            var font = $(".dashData.flowData li").css("font-family");
+            var options =  {
+              title: 'SIGNED CONTRACTS VALUE',
+//              curveType: 'function',
+              colors:['#57c1e8', '#bf83b9'], // color of the lines
+              legend: {
+                position: 'top',
+                textStyle: {
+                  color: '#574F48',
+                  fontName: font
+                }
+              },
+              vAxis:{
+                ticks: [
+                  {v:1000000, f:'€1 000 000'},
+                  {v:2000000, f:'€2 000 000'},
+                  {v:3000000, f:'€3 000 000'},
+                  {v:4000000, f:'€4 000 000'}
+                ]
+              }
+            };
+            return $.extend(true, {}, baseOptions(), options);
+          }
+
+          function resultsOptions(data) {
+            var font = $(".dashData.flowData li").css("font-family");
+            var options =  {
+              title: 'RESULTS',
+//              curveType: 'function',
+              colors:['#57c1e8', '#bf83b9'], // color of the lines
+              legend: {
+                position: 'top',
+                textStyle: {
+                  color: '#574F48',
+                  fontName: font
+                }
+              },
+              vAxes:{
+                0 : {
+                  ticks: [
+                    {v:-3.0, f:'-300%'},
+                    {v:-1.5, f:'-150%'},
+                    {v:0, f:'0%'},
+                    {v:1.5, f:'150%'},
+                    {v:3.0, f:'300%'},
+                  ]
+                },
+                1 : {
+                  ticks: [
+                    {v:-150000, f:'-€150 000'},
+                    {v:0, f:'€0'},
+                    {v:150000, f:'€150 000'},
+                    {v:300000, f:'€300 000'},
+                    {v:450000, f:'€450 000'},
+                  ]
+                }
+              },
+              series: {
+                0: {targetAxisIndex:0},
+                1: {targetAxisIndex:1}
+              }
+            };
+            return $.extend(true, {}, baseOptions(), options);
+          }
         }
+
       </script>
+
+      <li class="dashSingle" id="flowChart"></li>
+
+      <li class="dashSingle" id="contractChart"></li>
+
+      <li class="dashSingle" id="resultsChart"></li>
+
 
       <li class="dashSingle" id="opendaidDash">
         <h2>Akvo Openaid</h2>
@@ -182,7 +326,7 @@
     <div class="wrapper">
       <div class= "akvo_map centerED" id="akvo_map" style="width:975px;height:600px;"></div>
     </div>
-    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script> 
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
     <script type="text/javascript">
     var googleMap = {
       canvas: document.getElementById('akvo_map'),
@@ -245,7 +389,7 @@
       }
     };
     window.onload = function (){googleMap.load()};
-  </script> 
+  </script>
   </section>
 </div>
 <!-- end content -->
